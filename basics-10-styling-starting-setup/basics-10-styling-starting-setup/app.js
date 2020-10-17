b15084d19f7bcf31b4987f7d33d409d2bffcae71
dmarcr1997 <dmarcr1997@gmail.com>
2020-10-16 22:25:22 -0500

    dynamically styled toggle page

---
+++ basics-10-styling-starting-setup/basics-10-styling-starting-setup/app.js
@@ -0,0 +1,28 @@
+const app = new Vue({
+    el: '#styling',
+    data() {
+        return {
+            boxASelected: false,
+            boxBSelected: false,
+            boxCSelected: false,
+
+        }
+    },
+    methods: {
+        boxSelected(box) {
+            switch (box) {
+                case 'A':
+                    this.boxASelected = !this.boxASelected;
+                    break;
+                case 'B':
+                    this.boxBSelected = !this.boxBSelected;
+                    break;
+                case 'C':
+                    this.boxCSelected = !this.boxCSelected;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+})
